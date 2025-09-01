f51633a1255ca700cde2cc355e824db7ad9c636f
Sayantan Pal <sayantanpal100@LHPVTGPW0W.local>
2025-09-01 15:15:33 +0530

    2_returning values written in params

--- main.py
+++ main.py
@@ -4,31 +4,31 @@ app = FastAPI()
 
 @app.get("/")
 def index():
-    return {"Hello": "World"}
+    return {"Data": "Blog list"}
 
 
-
+@app.get('/blog/{id}')
+def show(id):
+    #here the type of id  is not specified so by default it becomes string
+    return {"Data": id}
 
 """
-☁  Fast-Api-tutorials-by-bitfumes-youtube [main] ⚡  uvicorn main:app --reload
+☁  Fast-Api-tutorials-by-bitfumes-youtube [main] uvicorn main:app --reload
 INFO:     Will watch for changes in these directories: ['/Users/sayantanpal100/Desktop/Udemy Projects/Fast-Api-tutorials-by-bitfumes-youtube']
 INFO:     Uvicorn running on http://127.0.0.1:8000 (Press CTRL+C to quit)
-INFO:     Started reloader process [7611] using StatReload
-INFO:     Started server process [7613]
+INFO:     Started reloader process [35486] using StatReload
+INFO:     Started server process [35489]
 INFO:     Waiting for application startup.
 INFO:     Application startup complete.
-INFO:     127.0.0.1:49566 - "GET / HTTP/1.1" 404 Not Found
-WARNING:  StatReload detected changes in 'main.py'. Reloading...
-INFO:     Shutting down
-INFO:     Waiting for application shutdown.
-INFO:     Application shutdown complete.
-INFO:     Finished server process [7613]
-INFO:     Started server process [9114]
-INFO:     Waiting for application startup.
-INFO:     Application startup complete.
-INFO:     127.0.0.1:50384 - "GET / HTTP/1.1" 200 OK
-INFO:     127.0.0.1:50394 - "GET / HTTP/1.1" 200 OK
-INFO:     127.0.0.1:50394 - "GET / HTTP/1.1" 200 OK
+INFO:     127.0.0.1:62241 - "GET / HTTP/1.1" 200 OK
+INFO:     127.0.0.1:62281 - "GET /blog HTTP/1.1" 404 Not Found
+INFO:     127.0.0.1:62328 - "GET /blog/200 HTTP/1.1" 200 OK
+INFO:     127.0.0.1:62361 - "GET /blog/sdfsfsfgvsfvsdf HTTP/1.1" 200 OK
+
+
+{
+"Data": "sdfsfsfgvsfvsdf"
+}
 
 
 
