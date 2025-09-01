ff70192deddd8e755f337419d20f667410b9f302
Sayantan Pal <sayantanpal100@LHPVTGPW0W.local>
2025-09-01 16:23:37 +0530

    4_type of id  is specified to be int

--- main.py
+++ main.py
@@ -8,36 +8,48 @@ def index():
 
 
 @app.get('/blog/{id}')
-def show(id):
-    #here the type of id  is not specified so by default it becomes string
+def show(id: int):
+    #here the type of id  is specified to be int
+    # Fetch blog post by id
     return {"Data": id}
 
-@app.get('/blog/{id}/comments')
-def comments(id):
-    return {"Data": f"Comments for blog {"1","2"}"}
+
 
 """
 ☁  Fast-Api-tutorials-by-bitfumes-youtube [main] uvicorn main:app --reload
 INFO:     Will watch for changes in these directories: ['/Users/sayantanpal100/Desktop/Udemy Projects/Fast-Api-tutorials-by-bitfumes-youtube']
 INFO:     Uvicorn running on http://127.0.0.1:8000 (Press CTRL+C to quit)
-INFO:     Started reloader process [35486] using StatReload
-INFO:     Started server process [35489]
+INFO:     Started reloader process [74075] using StatReload
+INFO:     Started server process [74077]
 INFO:     Waiting for application startup.
 INFO:     Application startup complete.
-INFO:     127.0.0.1:62241 - "GET / HTTP/1.1" 200 OK
-INFO:     127.0.0.1:62281 - "GET /blog HTTP/1.1" 404 Not Found
-INFO:     127.0.0.1:62328 - "GET /blog/200 HTTP/1.1" 200 OK
-INFO:     127.0.0.1:62361 - "GET /blog/sdfsfsfgvsfvsdf HTTP/1.1" 200 OK
+INFO:     127.0.0.1:65195 - "GET /item_type/134jdnvjhbenfbvdfl HTTP/1.1" 404 Not Found
+INFO:     127.0.0.1:65221 - "GET / HTTP/1.1" 200 OK
+INFO:     127.0.0.1:65264 - "GET /1003949 HTTP/1.1" 404 Not Found
+INFO:     127.0.0.1:65369 - "GET /blog/1003949 HTTP/1.1" 200 OK
+
 
 
 {
-"Data": "sdfsfsfgvsfvsdf"
+"Data": 1003949
 }
 
-INFO:     127.0.0.1:49771 - "GET /blog/sdfsfsfgvsfvsdf/comments HTTP/1.1" 200 OK
+
+INFO:     127.0.0.1:49263 - "GET /blog/1003949hjggfhgfgchfc HTTP/1.1" 422 Unprocessable Content
+
 
 {
-"Data": "Comments for blog ('1', '2')"
+"detail": [
+    {
+        "type": "int_parsing",
+        "loc": [
+            "path",
+            "id"
+        ],
+        "msg": "Input should be a valid integer, unable to parse string as an integer",
+        "input": "1003949hjggfhgfgchfc"
+    }
+]
 }
 
 """
