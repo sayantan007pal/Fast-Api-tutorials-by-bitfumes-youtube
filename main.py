7fc80b1980c8a02cdcd501e9a3ea5223369ceee5
Sayantan Pal <sayantanpal100@Mac.Vortex.local>
2025-09-02 02:13:58 +0530

    8_creating default params and optional query params

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 from fastapi import FastAPI
+from typing import Optional
 
 app = FastAPI()
 
 @app.get("/blogs")
-def index(limit:int, published: bool):
+def index(limit:int=10, published: Optional[bool]=None):    # here for the query parameters like limit and published, limit is set to 10 by default and published is optional so if any value of limit is given then it will take that value otherwise it will take the default value and for published if no value is given then it will take the default value of None
     if published:
         return {"Data": f"Published blog posts with limit {limit}"}
     else:
@@ -16,37 +17,33 @@ def index(limit:int, published: bool):
 ☁  Fast-Api-tutorials-by-bitfumes-youtube [main] uvicorn main:app --reload
 
 
-INFO:     127.0.0.1:53505 - "GET /blogs?limit=47&published=true HTTP/1.1" 200 OK
+INFO:     127.0.0.1:58467 - "GET /blogs?limit=23&published=false HTTP/1.1" 200 OK
 
 
 {
-"Data": "Published blog posts with limit 47"
+"Data": "Unpublished blog posts with limit 23"
 }
 
-INFO:     127.0.0.1:53735 - "GET /blogs?limit=47&published=false HTTP/1.1" 200 OK
+
+
+INFO:     127.0.0.1:58765 - "GET /blogs?published=false HTTP/1.1" 200 OK
 
 
 {
-"Data": "Unpublished blog posts with limit 47"
-}        
+"Data": "Unpublished blog posts with limit 10"
+}     
 
 
-INFO:     127.0.0.1:54311 - "GET /blogs?limit=47 HTTP/1.1" 422 Unprocessable Content
+INFO:     127.0.0.1:58995 - "GET /blogs?limit=23 HTTP/1.1" 200 OK
 
 
 {
-"detail": [
-{
-"type": "missing",
-"loc": [
-"query",
-"published"
-],
-"msg": "Field required",
-"input": null
+"Data": "Unpublished blog posts with limit 23"
 }
-]
-}
+
+
+
+
 
 """
 
