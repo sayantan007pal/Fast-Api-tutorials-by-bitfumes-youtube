b40d40f05ac842e823fad3a0d8f875cf8463d136
Sayantan Pal <sayantanpal100@LHPVTGPW0W.local>
2025-09-01 16:29:38 +0530

    5_path operations are evaluated in order

--- main.py
+++ main.py
@@ -13,29 +13,16 @@ def show(id: int):
     # Fetch blog post by id
     return {"Data": id}
 
-
+@app.get('/blogs/unpublished')
+def unpublished():
+    # Fetch unpublished blog posts
+    return {"Data": "Unpublished blog posts"}
 
 """
 ☁  Fast-Api-tutorials-by-bitfumes-youtube [main] uvicorn main:app --reload
-INFO:     Will watch for changes in these directories: ['/Users/sayantanpal100/Desktop/Udemy Projects/Fast-Api-tutorials-by-bitfumes-youtube']
-INFO:     Uvicorn running on http://127.0.0.1:8000 (Press CTRL+C to quit)
-INFO:     Started reloader process [74075] using StatReload
-INFO:     Started server process [74077]
-INFO:     Waiting for application startup.
-INFO:     Application startup complete.
-INFO:     127.0.0.1:65195 - "GET /item_type/134jdnvjhbenfbvdfl HTTP/1.1" 404 Not Found
-INFO:     127.0.0.1:65221 - "GET / HTTP/1.1" 200 OK
-INFO:     127.0.0.1:65264 - "GET /1003949 HTTP/1.1" 404 Not Found
-INFO:     127.0.0.1:65369 - "GET /blog/1003949 HTTP/1.1" 200 OK
 
 
-
-{
-"Data": 1003949
-}
-
-
-INFO:     127.0.0.1:49263 - "GET /blog/1003949hjggfhgfgchfc HTTP/1.1" 422 Unprocessable Content
+INFO:     127.0.0.1:53877 - "GET /blog/unpublished HTTP/1.1" 422 Unprocessable Content
 
 
 {
@@ -47,9 +34,18 @@ INFO:     127.0.0.1:49263 - "GET /blog/1003949hjggfhgfgchfc HTTP/1.1" 422 Unproc
             "id"
         ],
         "msg": "Input should be a valid integer, unable to parse string as an integer",
-        "input": "1003949hjggfhgfgchfc"
+        "input": "unpublished"
     }
 ]
 }
 
+"""
+
+"""
+here error is coming as fastAPI read routes like a ladder i.e
+ 1. It checks for exact matches first
+ 2. If no exact match is found, it checks for path parameters
+ 3. Finally, it checks for query parameters
+
+thus the error is occurring because FastAPI is trying to match the request path "/blog/unpublished" with the defined routes "/blog/{id}" and is getting confused because "unpublished" is not a valid integer.
 """
