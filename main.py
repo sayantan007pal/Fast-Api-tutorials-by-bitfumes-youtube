ace7086be3cf8e75020a076e1f2fde4f38588e94
Sayantan Pal <sayantanpal100@Mac.Vortex.local>
2025-09-02 01:59:16 +0530

    7_giving inputs using query params

--- main.py
+++ main.py
@@ -2,21 +2,13 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-@app.get("/")
-def index():
-    return {"Data": "Blog list"}
+@app.get("/blogs")
+def index(limit:int, published: bool):
+    if published:
+        return {"Data": f"Published blog posts with limit {limit}"}
+    else:
+        return {"Data": f"Unpublished blog posts with limit {limit}"}
 
-
-@app.get('/blogs/unpublished')
-def unpublished():
-    # Fetch unpublished blog posts
-    return {"Data": "Unpublished blog posts"}
-
-@app.get('/blogs/{id}')
-def show(id: int):
-    #here the type of id  is specified to be int
-    # Fetch blog post by id
-    return {"Data": id}
 
 
 
@@ -24,18 +16,37 @@ def show(id: int):
 ☁  Fast-Api-tutorials-by-bitfumes-youtube [main] uvicorn main:app --reload
 
 
-INFO:     127.0.0.1:56602 - "GET /blogs/unpublished HTTP/1.1" 200 OK
+INFO:     127.0.0.1:53505 - "GET /blogs?limit=47&published=true HTTP/1.1" 200 OK
 
 
 {
-"Data": "Unpublished blog posts"
+"Data": "Published blog posts with limit 47"
+}
+
+INFO:     127.0.0.1:53735 - "GET /blogs?limit=47&published=false HTTP/1.1" 200 OK
+
+
+{
+"Data": "Unpublished blog posts with limit 47"
+}        
+
+
+INFO:     127.0.0.1:54311 - "GET /blogs?limit=47 HTTP/1.1" 422 Unprocessable Content
+
+
+{
+"detail": [
+{
+"type": "missing",
+"loc": [
+"query",
+"published"
+],
+"msg": "Field required",
+"input": null
+}
+]
 }
 
 """
 
-"""
-Now the API is able to correctly identify the route for unpublished blog posts.
-as the request path "/blogs/unpublished" matches the defined route 
-as while running  the FastAPI application, it checks for exact matches first thus 
- path "/blogs/unpublished" is checked before the "blogs/{id}" route is checked.
-"""
